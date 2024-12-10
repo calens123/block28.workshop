42f51a2ec2f6a33f21419e8663145c9a4feaac9b
Calen Sturtevant <calen.s@icloud.com>
2024-12-09 16:14:04 -0800

    submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import Blue from "./components/Blue";
 import Red from "./components/Red";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Green from "./components/Green";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div id="container">
-      <div id="navbar">
-        <Link to="/blue">Blue</Link>
-        <Link to="/red">Red</Link>
-      </div>
+      <Navbar />
       <div id="main-section">
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/blue" element={<Blue />} />
           <Route path="/red" element={<Red />} />
+          <Route path="/green" element={<Green />} />
         </Routes>
       </div>
+      <Footer />
     </div>
   );
 }
